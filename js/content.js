cd6aba54601306e765ed414c5a97008fed43e383
Shreeji Pedhadiya <shreejilucifer@gmail.com>
2019-01-02 10:40:15 +0530

    Fighting with Media Queries

--- js/content.js
+++ js/content.js
@@ -4,7 +4,7 @@ var nontechevents = {
   "events": [{
       "name": "Maths Guru",
       "event": "maths-guru",
-
+      
       "judgingcriteria": "Lorem Ipsum is simply dumEvent Co-ordinator Name text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dumEvent Co-ordinator Name text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
       "rounds": {
         "round1": {
@@ -2213,15 +2213,15 @@ var civil = {
 window.siteContent = {
   "current_department": "1",
   "departments": [
-    nontechevents,
+    nontechevents, 
     ecell,
-    informationtech,
-    mechanical,
-    fpt,
-    electronics,
+    informationtech, 
+    mechanical, 
+    fpt, 
+    electronics, 
     electrical,
-    computer,
-    auto,
+    computer,  
+    auto, 
     civil
   ],
   "spotify_links": {
